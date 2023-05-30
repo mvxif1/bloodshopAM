ad6bfb1897d27cb2f65fb0e41cbabf0ce9519eb5
elxumare1 <118561281+elxumare1@users.noreply.github.com>
2023-05-30 00:48:54 -0400

    2311

--- bloodshop/core/views.py
+++ bloodshop/core/views.py
@@ -20,17 +20,83 @@ def detailsgirl2(request):
 def detailsgirl3(request):
     return render(request. 'core/detailsgirl3.html')
     
-def adminshoes(request):
-    return render(request. 'core/adminhoes.html')
+def detailsgirl4(request):
+    return render(request. 'core/detailsgirl4.html')
     
-def adminshoes(request):
-    return render(request. 'core/adminhoes.html')
+def detailsgirl5(request):
+    return render(request. 'core/detailsgirl5.html')
     
-def adminshoes(request):
-    return render(request. 'core/adminhoes.html')
+def detailsgirl6(request):
+    return render(request. 'core/detailsgirl6.html')
     
-def adminshoes(request):
-    return render(request. 'core/adminhoes.html')
+def detailsmen1(request):
+    return render(request. 'core/detailsmen1.html')
     
-def adminshoes(request):
-    return render(request. 'core/adminhoes.html')
+def detailsmen2(request):
+    return render(request. 'core/detailsmen2.html')
+
+def detailsmen3(request):
+    return render(request. 'core/detailsmen3.html')
+
+def detailsmen4(request):
+    return render(request. 'core/detailsmen4.html')
+
+def detailsmen5(request):
+    return render(request. 'core/detailsmen5.html')
+
+def detailsmen6(request):
+    return render(request. 'core/detailsmen6.html')
+ 
+def detailsninos1(request):
+    return render(request. 'core/detailsninos1.html')    
+
+def detailsninos2(request):
+    return render(request. 'core/detailsninos2.html')
+
+def detailsninos3(request):
+    return render(request. 'core/detailsninos3.html')
+
+def detailsninos4(request):
+    return render(request. 'core/detailsninos4.html')
+
+def detailsninos5(request):
+    return render(request. 'core/detailsninos5.html')
+
+def detailsninos6(request):
+    return render(request. 'core/detailsninos6.html')
+
+def hombre(request):
+    return render(request. 'core/hombre.html')
+    
+def hombreadmin(request):
+    return render(request. 'core/hombreadmin.html')
+    
+def inicio(request):
+    return render(request. 'core/inicio.html')
+    
+def inicioadmin(request):
+    return render(request. 'core/inicioadmin.html')
+
+def iniciobloodshop(request):
+    return render(request. 'core/iniciobloodshop.html')
+    
+def iniciobloodshopadmin(request):
+    return render(request. 'core/iniciobloodshopadmin.html')
+    
+def mujer(request):
+    return render(request. 'core/mujer.html')
+        
+def mujeradmin(request):
+    return render(request. 'core/mujeradmin.html')
+
+def ninos(request):
+    return render(request. 'core/ninos.html')
+    
+def ninosadmin(request):
+    return render(request. 'core/ninosadmin.html')
+    
+def olvidepassword(request):
+    return render(request. 'core/olvidepassword.html')
+
+def register(request):
+    return render(request. 'core/register.html')
