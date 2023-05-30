ae7029a765efc3de6ce97839a69191d5243001a5
elxumare1 <118561281+elxumare1@users.noreply.github.com>
2023-05-30 00:26:25 -0400

    añadir todo

---
+++ bloodshop/core/views.py
@@ -0,0 +1,36 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def adminshoes(request):
+    return render(request. 'core/adminhoes.html')
+    
+def carrito(request):
+    return render(request. 'core/carrito.html')
+    
+def details(request):
+    return render(request. 'core/details.html')
+    
+def detailsgirl1(request):
+    return render(request. 'core/detailsgirl1.html')
+    
+def detailsgirl2(request):
+    return render(request. 'core/detailsgirl2.html')
+    
+def detailsgirl3(request):
+    return render(request. 'core/detailsgirl3.html')
+    
+def adminshoes(request):
+    return render(request. 'core/adminhoes.html')
+    
+def adminshoes(request):
+    return render(request. 'core/adminhoes.html')
+    
+def adminshoes(request):
+    return render(request. 'core/adminhoes.html')
+    
+def adminshoes(request):
+    return render(request. 'core/adminhoes.html')
+    
+def adminshoes(request):
+    return render(request. 'core/adminhoes.html')
