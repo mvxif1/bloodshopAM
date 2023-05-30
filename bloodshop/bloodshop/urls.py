b79ad98d76cadaa58968cca2d6860cdd2e8ebb79
elxumare1 <118561281+elxumare1@users.noreply.github.com>
2023-05-30 00:54:36 -0400

    urls.py

--- bloodshop/bloodshop/urls.py
+++ bloodshop/bloodshop/urls.py
@@ -15,11 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from .views import adminshoes, carrito, details, detailsgirl1, detailsgirl3, detailsgirl4, detailsgirl5, detailsgirl6,
-detailsmen1, detailsmen2, detailsmen3, detailsmen4, detailsmen5, detailsmen6, detailsninos1, detailsninos2, detailsninos3,
-detailsninos4, detailsninos5, detailsninos6, hombre, hombreadmin, inicio, inicioadmin, iniciobloodshop, iniciobloodshopadmin,
-mujer, mujeradmin, ninos, ninosadmin, olvidepassword, register
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+
