14e5e06e709bd6ce70292eeecb6a72492bca5fd1
elxumare1 <118561281+elxumare1@users.noreply.github.com>
2023-05-30 01:20:02 -0400

    213

--- bloodshop/core/urls.py
+++ bloodshop/core/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import adminshoes, carrito, details, detailsgirl1, detailsgirl2, detailsgirl3, detailsgirl4, detailsgirl5, detailsgirl6, detailsmen1, detailsmen2, detailsmen3, detailsmen4, detailsmen5, detailsmen6, detailsninos1, detailsninos2, detailsninos3, detailsninos4, detailsninos5, detailsninos6, hombre, hombreadmin, inicio, inicioadmin, iniciobloodshop, iniciobloodshopadmin, mujer, mujeradmin, ninos, ninosadmin, olvidepassword, register
 
 urlpatterns = {
-    path('', iniciobloodshop, name="home"),
+    path('', iniciobloodshop, name="iniciobloodshop"),
     path('carrito', carrito, name="carrito"),
     path('detailsgirl1', detailsgirl1, name="detailsgirl1"),
     path('detailsgirl2', detailsgirl2, name="detailsgirl2"),
